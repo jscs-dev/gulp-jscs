40bbe7063909f073635f0e8017e765aa3c9d6e8a
UltCombo <ultcombo@gmail.com>
2015-02-08 16:07:42 -0200

    Reporters: add tests

--- test.js
+++ test.js
@@ -68,7 +68,7 @@ it('should check code style of JS files using a preset', function (cb) {
 it('should pass valid files', function (cb) {
 	var stream = jscs();
 
-	stream.pipe(jscs.reporter()).pipe(jscs.reporter('fail')).on('error', function (err) {
+	stream.pipe(jscs.reporter('fail')).on('error', function (err) {
 		assert(false, err);
 	}).on('end', cb).resume();
 
@@ -83,7 +83,7 @@ it('should pass valid files', function (cb) {
 it('should respect "excludeFiles" from config', function (cb) {
 	var stream = jscs();
 
-	stream.pipe(jscs.reporter()).pipe(jscs.reporter('fail')).on('error', function (err) {
+	stream.pipe(jscs.reporter('fail')).on('error', function (err) {
 		assert(false, err);
 	}).on('end', cb).resume();
 
@@ -152,4 +152,79 @@ it('should not mutate the options object passed as argument', function () {
 	var options = {esnext: true};
 	jscs(options);
 	assert.equal(options.esnext, true);
+});
+
+describe('Reporter', function () {
+	it('`.reporter()` called with no arguments should use the default reporter', function (cb) {
+		stubStdout();
+		var stream = jscs();
+
+		stream.pipe(jscs.reporter()).on('end', function (err) {
+			assert(/Multiple var declaration[^]*---\^/.test(stdoutStub));
+			teardown();
+			cb();
+		}).resume();
+
+		stream.write(new gutil.File({
+			base: __dirname,
+			path: __dirname + '/fixture.js',
+			contents: new Buffer('var x = 1,y = 2;')
+		}));
+
+		stream.end();
+	});
+
+	it('should accept a built-in JSCS reporter name', function (cb) {
+		stubStdout();
+		var stream = jscs();
+
+		stream.pipe(jscs.reporter('inlinesingle')).on('end', function (err) {
+			assert(/line 1, col 0, Multiple var declaration/.test(stdoutStub));
+			teardown();
+			cb();
+		}).resume();
+
+		stream.write(new gutil.File({
+			base: __dirname,
+			path: __dirname + '/fixture.js',
+			contents: new Buffer('var x = 1,y = 2;')
+		}));
+
+		stream.end();
+	});
+
+	it('`fail` reporter should throw an error', function (cb) {
+		var stream = jscs();
+
+		stream.pipe(jscs.reporter('fail')).on('error', function (/* some args here so can't pass cb directly */) {
+			cb();
+		}).resume();
+
+		stream.write(new gutil.File({
+			base: __dirname,
+			path: __dirname + '/fixture.js',
+			contents: new Buffer('var x = 1,y = 2;')
+		}));
+
+		stream.end();
+	});
+
+	it('should accept a function', function (cb) {
+		function reporterFn(errors) {
+			assert(/Multiple var declaration/.test(errors[0].explainError(errors[0].getErrorList()[0], false)));
+			cb();
+		}
+
+		var stream = jscs();
+
+		stream.pipe(jscs.reporter(reporterFn)).resume();
+
+		stream.write(new gutil.File({
+			base: __dirname,
+			path: __dirname + '/fixture.js',
+			contents: new Buffer('var x = 1,y = 2;')
+		}));
+
+		stream.end();
+	});
 });
